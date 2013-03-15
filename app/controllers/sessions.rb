e4b3dc3b7cbdc28a6c4aaab8d1f5c8df3d9c9112
Justin Ramel <justin.ramel@gmail.com>
2013-03-15 23:30:32 +0000

    adding bootstrap styled flash messages partial

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,35 +1,16 @@
 YellowJersey.controllers :sessions do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
 
   get :new do
-    render 'sessions/new'
+    render url(:sessions, :new)
   end
 
-  
   post :create do
     if account = Account.authenticate(params[:email], params[:password])
       set_current_account(account)
-      redirect url("/home")
+      redirect url(:home, :index)
     else
       params[:email], params[:password] = h(params[:email]), h(params[:password])
-      flash[:warning] = "Login or password wrong."
+      flash[:error] = "Wrong email or password."
       redirect url(:sessions, :new)
     end
   end
