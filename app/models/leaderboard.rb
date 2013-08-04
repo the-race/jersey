7d9448f3d1d34c5b1464ca775718e503c02f8ba4
Justin Ramel <justin.ramel@gmail.com>
2013-08-04 23:34:18 +0100

    adding commas to totals in the thousands

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -1,11 +1,15 @@
 Leaderboard = Struct.new(:name, :total, :behind) do
-  FLOAT_FORMAT = '%.2f'
-
   def total_to_s
-   FLOAT_FORMAT % total
+   pretty_float(total)
   end
 
   def behind_to_s
-   behind == '-' ? behind : FLOAT_FORMAT % behind
+   behind == '-' ? behind : pretty_float(behind)
+  end
+
+  private
+
+  def pretty_float(st)
+    ('%.2f' % st).gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
   end
 end
