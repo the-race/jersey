7da9c58b5229ff8d1edc56b0c13a9eabb898e0f0
Justin Ramel <justin.ramel@gmail.com>
2013-08-09 21:39:28 +0100

    updating config for deploy to linux

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,54 @@
 require "bundler/capistrano"
-
-set :application, "jersey"
-set :user, "njr11"
-
-
-set :scm, :git
-set :repository, "git@github.com:the-race/jersey.git"
-set :branch, "master"
-set :use_sudo, true
-
+require "rvm/capistrano"
 
 server "jersey.cloudapp.net", :web, :app, :db, primary: true
 
-
+set :application, "jersey"
+set :user, "njr11"
+set :port, 2056
 set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:the-race/jersey.git"
+set :branch, "master"
+
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:port] = 22
 
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-  desc "Fix permissions"
-  task :fix_permissions, :roles => [ :app, :db, :web ] do
-    run "chmod +x #{release_path}/config/unicorn_init.sh"
-  end
-
-
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "service unicorn_#{application} #{command}"
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
-
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    sudo "mkdir -p #{shared_path}/config"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
-
   task :symlink_config, roles: :app do
-    # Add database config here
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
-  after "deploy:finalize_update", "deploy:fix_permissions"
   after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,15 @@
-worker_processes 2
-timeout 30
-listen "/tmp/unicorn.jersey.sock"
-
 root = "/home/njr11/apps/jersey/current"
-
 working_directory root
-
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.jersey.sock"
+worker_processes 2
+timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
